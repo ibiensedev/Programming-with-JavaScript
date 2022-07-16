e773f1c7cbe0c2fabc746583dbbd2417f757b807
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-16 17:45:15 +0200

    Numbers theory

---
+++ Writing-your-first-JavaScript-code/Numbers-Theory.js
@@ -0,0 +1,9 @@
+console.log(2 + 2);
+console.log(4 - 2);
+console.log(4 * 4);
+console.log(16 / 4);
+console.log(10 ** 2);
+console.log(9 % 8);
+console.log(16 % 8);
+console.log(2 * 4 + 8);
+console.log(2 * (4 + 8));
