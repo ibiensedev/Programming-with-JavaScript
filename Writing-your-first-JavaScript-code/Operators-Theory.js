0b12045ee0ba4062031f6c77f7d2651fb3d24d8d
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-16 17:37:31 +0200

    operators theory

---
+++ Writing-your-first-JavaScript-code/Operators-Theory.js
@@ -0,0 +1,14 @@
+/*
+Assignment operator: =
+Arithmetic operators: +, -, /, *
+Comparison operators: >, <, ==, !=
+Logical operators: &&, ||, !
+*/
+
+console.log(2 + 2);
+console.log(20 - 18);
+console.log(2 * 3)
+console.log(8 / 1);
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
