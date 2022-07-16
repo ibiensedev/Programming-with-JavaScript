700eb05cacd11d810ffd6bab261ccd1ac032035c
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-16 18:59:27 +0200

    operators in depth

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Programming-with-JavaScript
+
+LINKS OF INTEREST
+
+ECMA262 Specification: https://tc39.es/ecma262/
+jQuery Official Site: https://jquery.com/
+React Official Site: https://reactjs.org/
+Stack OverFlow Developer Survey 2021 https://insights.stackoverflow.com/survey/2021#
+Emojis http://unicode.org/emoji/charts/full-emoji-list.html#1f600
+
---
+++ Writing-your-first-JavaScript-code/Operators-in-depth.js
@@ -0,0 +1,45 @@
+/*
+Logical AND operator: && 
+console.log(true && true) will output: true 
+console.log(true && false) will output: false 
+console.log(false && true) will output: false 
+console.log(false && false) will output: false
+
+Logical OR operator: || 
+console.log(true || true) will output: true 
+console.log(true || false) will output: true  
+console.log(false || true) will output: true  
+console.log(false || false) will output: false
+
+Logical NOT operator: ! 
+
+The modulus operator: % 
+
+The equality operator: ==
+
+The strict equality operator: ===
+
+The inequality operator: !=
+
+The strict inequality operator: !==
+
+The addition assignment operator: += -> to number
+
+The concatenation assignment operator: += -> to string
+*/
+
+var petHungry = true;
+
+console.log(petHungry);
+console.log(!petHungry);
+
+var overtime = 1;
+overtime += 2;
+overtime -= 2;
+console.log(overtime);
+
+var longString = "";
+longString += "Hello ";
+longString += "World";
+longString += "!";
+console.log(longString);
