20a47337f2eda29baf1ddf5aa4cc922a9eee42f6
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-16 18:59:03 +0200

    booleans theory

---
+++ Writing-your-first-JavaScript-code/Booleans-Theory.js
@@ -0,0 +1,5 @@
+console.log(100 == "100"); //the value is the same
+console.log(100 === "100"); //the type is not the same -> strict equality operator
+
+console.log(1 != "1");
+console.log(1 !== "1"); //the type is not the same -> strict inequality operator
