bdb904717fc79dc5450dab1fc95ad6c657020e68
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-16 17:24:08 +0200

    data types theory

---
+++ Writing-your-first-JavaScript-code/Data-Types-Theory.js
@@ -0,0 +1,14 @@
+/*
+There are seven primitive data yypes in JavaScript:
+
+String -> 'hello world!'
+Number -> 375
+Boolean -> True, False
+Null -> represents the abscence of value
+Undefined -> refers to a variable that has not yet been assigned a value
+
+ES6 introduced two new primitive data types:
+
+BigInt -> very large range of numbers
+Symbol -> can be used as a unique identifier
+*/
