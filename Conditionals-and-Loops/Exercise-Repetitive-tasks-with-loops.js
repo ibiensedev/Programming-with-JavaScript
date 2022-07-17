690ccf890a2a140d143d6e4a4c397eb10f1ee446
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-17 15:58:41 +0200

    exercise loops

---
+++ Conditionals-and-Loops/Exercise-Repetitive-tasks-with-loops.js
@@ -0,0 +1,42 @@
+// Task 1
+
+for (let index = 1; index <= 5; index++) {
+    console.log(index);
+}
+console.log("Counting completed!");
+
+// Task 2 
+
+for (let index = 5; index > 0; index--) {
+    console.log(index);
+}
+console.log("Countdown finished!");
+
+// Task 3
+
+var index1 = 1;
+
+while (index1 < 6) {
+    console.log(index1);
+    index1++;
+}
+console.log("Counting completed!");
+
+// Task 4
+
+var index2 = 5;
+
+while (index2 > 0) {
+    console.log(index2);
+    index2--;
+}
+console.log("Countdown finished!");
+
+//Task 5
+
+var year = 2018;
+
+while (year < 2023) {
+    console.log(year);
+    year++;
+}
