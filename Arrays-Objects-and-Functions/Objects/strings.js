f6151c06b07b01461dc92174af78ddd532a91c95
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-24 18:24:27 +0200

    strings

---
+++ Arrays-Objects-and-Functions/Objects/strings.js
@@ -0,0 +1,16 @@
+var greet = "Hello-Hello";
+var place = "world";
+
+console.log(greet.length);
+console.log(greet.charAt(0));
+
+console.log(greet.concat(" " + place));
+
+//returns the location of a caracter
+console.log(greet.indexOf("H"));
+
+console.log(greet.split("-"));
+
+console.log(greet.toUpperCase());
+
+console.log(greet.toLowerCase());
