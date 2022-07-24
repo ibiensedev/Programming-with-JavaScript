d0f258954042f651a47281f49c9f89bccbce9f66
CORP\mferna27 <marcos.fernandez-sanchez-external@capgemini.com>
2022-07-24 18:58:22 +0200

    exercise creating arrays and methods

---
+++ Arrays-Objects-and-Functions/Objects/Exercise-Creating-arrays-and-objects.js
@@ -0,0 +1,23 @@
+var clothes = [];
+
+clothes.push("favorite1");
+clothes.push("favorite2");
+clothes.push("favorite3");
+clothes.push("favorite4");
+clothes.push("favorite5");
+
+clothes.pop();
+
+clothes.push("favorite5");
+
+console.log(clothes);
+
+var favCar = {
+
+}
+
+favCar.color = "blue";
+favCar.convertible = true;
+
+console.log(favCar);
+
